57d0d9d294365a1b6097c23aaa993041b230d802
Captain-ExE <162823058+Captain-ExE@users.noreply.github.com>
2024-07-09 00:09:34 +0600

    Create wordList.py
    list of words.

---
+++ Hangman-game/wordList.py
@@ -0,0 +1,8 @@
+word_list = [
+    'good', 'bad', 'happy', 'sad', 'angry', 'tired', 'hungry', 'thirsty',
+    'hot', 'cold', 'wet', 'dry', 'clean', 'dirty', 'big', 'small', 'long',
+    'short', 'tall', 'fast', 'slow', 'light', 'heavy', 'loud', 'quiet', 'easy',
+    'hard', 'new', 'old', 'red', 'blue', 'green', 'yellow', 'purple', 'white',
+    'black', 'brown', 'gray', 'pink', 'orange', 'sun', 'moon', 'star', 'tree',
+    'bird', 'fish', 'dog', 'cat', 'house', 'car', 'book', 'chair'
+]
